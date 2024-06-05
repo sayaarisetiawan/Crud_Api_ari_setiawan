1b4393282f3a1a571549a7b8c8319859937d36c4
Ari Setiawan <ari_22510010@stimata.ac.id>
2024-06-06 01:35:18 +0700

    Tugas Pertemuan 9
    CRUD API

---
+++ app/src/main/java/com/example/crud_api_ari_setiawan/ResultStaff.kt
@@ -0,0 +1,10 @@
+package com.example.crud_api_ari_setiawan
+import com.google.gson.annotations.SerializedName
+data class ResultStaff (
+    @field:SerializedName("pesan")
+    val pesan: String? = null,
+    @field:SerializedName("staff")
+    val staff: List<DataItem>? = null,
+    @field:SerializedName("status")
+    val status: Int? = null
+)
---
+++ app/src/main/java/com/example/crud_api_ari_setiawan/ResultStatus.kt
@@ -0,0 +1,6 @@
+package com.example.crud_api_ari_setiawan
+
+class ResultStatus {
+    val pesan : String? = null
+    val status : Int? = null
+}
